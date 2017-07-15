290723a9f225a4f16ec7c86dcfe5cbabdbaa7adc
Felix Werpers <felix@werpers.com>
2017-07-16 00:45:51 +0200

    Add log fragment

--- timeProf/src/main/java/com/fwerpers/timeprof/NavigationActivity.java
+++ timeProf/src/main/java/com/fwerpers/timeprof/NavigationActivity.java
@@ -1,5 +1,7 @@
 package com.fwerpers.timeprof;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -72,16 +74,16 @@ public class NavigationActivity extends AppCompatActivity {
     /** Swaps fragments in the main content view */
     private void selectItem(int position) {
         // Create a new fragment and specify the planet to show based on position
-//        Fragment fragment = new PlanetFragment();
+        Fragment fragment = new LogFragment();
 //        Bundle args = new Bundle();
-//        args.putInt(PlanetFragment.ARG_PLANET_NUMBER, position);
+//        args.putInt(LogFragment.ARG_PLANET_NUMBER, position);
 //        fragment.setArguments(args);
-//
-//        // Insert the fragment by replacing any existing fragment
-//        FragmentManager fragmentManager = getFragmentManager();
-//        fragmentManager.beginTransaction()
-//                .replace(R.id.content_frame, fragment)
-//                .commit();
+
+        // Insert the fragment by replacing any existing fragment
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.content_frame, fragment)
+                .commit();
 
         // Highlight the selected item, update the title, and close the drawer
         mDrawerList.setItemChecked(position, true);
