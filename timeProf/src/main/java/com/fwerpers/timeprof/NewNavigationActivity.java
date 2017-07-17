b7647bde6a92cba3f166c8b627cce036d58b6b25
Felix Werpers <felix@werpers.com>
2017-07-17 17:38:24 +0200

    Sett toggle listener and oncreate. Code from TPController

--- timeProf/src/main/java/com/fwerpers/timeprof/NewNavigationActivity.java
+++ timeProf/src/main/java/com/fwerpers/timeprof/NewNavigationActivity.java
@@ -1,7 +1,10 @@
 package com.fwerpers.timeprof;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.Intent;
-import android.preference.PreferenceFragment;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -10,11 +13,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class NewNavigationActivity extends AppCompatActivity {
 
+    public static final String KEY_RUNNING = "running";
+
     private NavigationView mNavigationView;
     private DrawerLayout mDrawerLayout;
+    private SharedPreferences mSettings;
+    public static boolean mRunning;
+
+    private ToggleButton mNotificationToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +35,24 @@ public class NewNavigationActivity extends AppCompatActivity {
 
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         mNavigationView = (NavigationView) findViewById(R.id.navigation);
+        mSettings = PreferenceManager.getDefaultSharedPreferences(this);
+        mRunning = mSettings.getBoolean(KEY_RUNNING, true);
+
+        mNotificationToggle = (ToggleButton) mNavigationView.getMenu().findItem(R.id.nav_notification_toggle).getActionView();
+        mNotificationToggle.setChecked(mRunning);
+        mNotificationToggle.setOnClickListener(mTogListener);
+
+        // TODO: verify that reinstall should be the only time
+        // that mRunning would be stored as "On" without having an alarm set
+        // for the next ping time...
+        // if (mRunning) {
+        Integer stored = Integer.parseInt(mSettings.getString("KEY_APP_VERSION", "-1"));
+        Integer manifest = Integer.parseInt(getText(R.string.app_version).toString());
+        if (stored < manifest || mSettings.getLong(PingService.KEY_NEXT, -1) < 0
+                || mSettings.getLong(PingService.KEY_SEED, -1) < 0) {
+            startService(new Intent(this, PingService.class));
+        }
+        // }
 
         //setting up selected item listener
         mNavigationView.setNavigationItemSelectedListener(
@@ -54,12 +84,41 @@ public class NewNavigationActivity extends AppCompatActivity {
                                 startSettingsActivity();
                                 mDrawerLayout.closeDrawers();
                                 break;
-                            case R.id.nav_notification_switch:
+                            case R.id.nav_notification_toggle:
                                 break;
                         }
                         return true;
                     }
                 });
+    }
+
+    private View.OnClickListener mTogListener = new View.OnClickListener() {
+        public void onClick(View v) {
+            SharedPreferences.Editor editor = mSettings.edit();
+
+            // Perform action on clicks
+            if (mNotificationToggle.isChecked()) {
+                Toast.makeText(NewNavigationActivity.this, "Pings ON", Toast.LENGTH_SHORT).show();
+                mRunning = true;
+                editor.putBoolean(KEY_RUNNING, mRunning);
+                setAlarm();
+            } else {
+                Toast.makeText(NewNavigationActivity.this, "Pings OFF", Toast.LENGTH_SHORT).show();
+                mRunning = false;
+                editor.putBoolean(KEY_RUNNING, mRunning);
+                cancelAlarm();
+            }
+            editor.commit();
+        }
+    };
+
+    public void setAlarm() {
+        startService(new Intent(this, PingService.class));
+    }
+
+    public void cancelAlarm() {
+        AlarmManager alarum = (AlarmManager) getSystemService(ALARM_SERVICE);
+        alarum.cancel(PendingIntent.getService(this, 0, new Intent(this, PingService.class), 0));
     }
 
     private void switchToFragment(Fragment fragment) {
