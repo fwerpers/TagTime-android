d2a6b482b3250153270f04d5645e1962a7c82202
Felix Werpers <felix@werpers.com>
2017-07-23 15:22:40 +0200

    Remove unused

--- timeProf/src/main/java/com/fwerpers/timeprof/Constants.java
+++
@@ -1,9 +0,0 @@
-package com.fwerpers.timeprof;
-
-/**
- * Created by FWerpers on 13/07/17.
- */
-
-public class Constants {
-    public static final String ACTION_GAP_CHANGED = "com.fwerpers.timeprof.ACTION_GAP_CHANGED";
-}
