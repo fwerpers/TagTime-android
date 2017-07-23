e95a65e34ab6e411a63541bc2b37b9ef1e5f4220
Felix Werpers <felix@werpers.com>
2017-07-23 14:00:15 +0200

    Add Constants (ended up in different package when merging)

---
+++ tagtime/src/main/java/com/fwerpers/tagtime/Constants.java
@@ -0,0 +1,9 @@
+package com.fwerpers.tagtime;
+
+/**
+ * Created by FWerpers on 23/07/17.
+ */
+
+public class Constants {
+    public static final String ACTION_GAP_CHANGED = "com.fwerpers.timeprof.ACTION_GAP_CHANGED";
+}
