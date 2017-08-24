fb92745056806f7ac2d1da5994e7b6a574bfce5e
Felix Werpers <felix@werpers.com>
2017-08-24 18:49:54 +0200

    Add debug boolean

--- tagtime/src/main/java/com/fwerpers/tagtime/Constants.java
+++ tagtime/src/main/java/com/fwerpers/tagtime/Constants.java
@@ -8,4 +8,5 @@ public class Constants {
     public static final String ACTION_GAP_CHANGED = "com.fwerpers.timeprof.ACTION_GAP_CHANGED";
     public static final String KEY_RUNNING = "running";
     public static final boolean DEBUG = false;
+    public static final boolean DEBUG_DATABASE_FREEZE = true;
 }
