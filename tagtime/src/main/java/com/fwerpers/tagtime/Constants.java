1544b876835f95a38b6fc7349e36beabe0e577fe
Felix Werpers <felix@werpers.com>
2017-09-01 21:54:17 +0200

    Set debug freeze to false

--- tagtime/src/main/java/com/fwerpers/tagtime/Constants.java
+++ tagtime/src/main/java/com/fwerpers/tagtime/Constants.java
@@ -8,5 +8,5 @@ public class Constants {
     public static final String ACTION_GAP_CHANGED = "com.fwerpers.timeprof.ACTION_GAP_CHANGED";
     public static final String KEY_RUNNING = "running";
     public static final boolean DEBUG = false;
-    public static final boolean DEBUG_DATABASE_FREEZE = true;
+    public static final boolean DEBUG_DATABASE_FREEZE = false;
 }
